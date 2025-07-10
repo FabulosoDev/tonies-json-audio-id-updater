3fbaab4eb8e9b77a0540cddef3270aa6421addd2
FabulosoDev <113933725+FabulosoDev@users.noreply.github.com>
2025-07-10 15:19:52 +0200

    - checkout latest commit of remote_update_branch

--- app/tonies_json_repo.py
+++ app/tonies_json_repo.py
@@ -42,7 +42,13 @@ class ToniesJsonRepo:
                 repo.git.reset('--hard', 'origin/master')
                 repo.git.clean('-fd')
 
-                repo.remotes.origin.pull(rebase=True)
+                remote_update_branch = f"origin/{self.tonies_json_update_branch}"
+                if remote_update_branch in [ref.name for ref in repo.refs]:
+                    repo.git.checkout('-B', self.tonies_json_update_branch, remote_update_branch)
+                    repo.git.reset('--hard', remote_update_branch)
+                else:
+                    repo.git.checkout('-B', self.tonies_json_update_branch)
+
             except GitCommandError as e:
                 logger.error(f"Failed to force pull repo: {e}")
 
@@ -70,7 +76,6 @@ class ToniesJsonRepo:
     def commit_changes(self):
         """Commit changes to YAML files, one commit per file."""
         repo = Repo(self.tonies_json_repo_path)
-        repo.git.checkout('-B', self.tonies_json_update_branch)
 
         if not repo.is_dirty(untracked_files=True):
             logger.info("No changes to commit.")
